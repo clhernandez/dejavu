5f036475c938da7be4419333767d829590fa2189
Claudio <claudio@zapping.com>
2024-05-16 18:44:10 -0400

    add len(hashes) and dir to fingerprint file

--- dejavu/__init__.py
+++ dejavu/__init__.py
@@ -133,12 +133,12 @@ class Dejavu:
         if song_hash in self.songhashes_set:
             print(f"{song_name} already fingerprinted, continuing...")
         else:
-            song_name, hashes, file_hash = Dejavu._fingerprint_worker(
+            song_name, hashes, file_hash, dir = Dejavu._fingerprint_worker(
                 file_path,
                 self.limit,
                 song_name=song_name
             )
-            sid = self.db.insert_song(song_name, file_hash)
+            sid = self.db.insert_song(song_name, file_hash, len(hashes), dir)
 
             self.db.insert_hashes(sid, hashes)
             self.db.set_song_fingerprinted(sid)
